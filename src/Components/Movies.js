d8c22e6f0fda9cfcc89619169dfb7cc2e22ff7c2
goelabhishek694 <goelabhishek694@gmail.com>
2021-06-26 18:55:24 +0530

    loader added

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -92,7 +92,9 @@
 
 
 import React, { Component } from 'react'
-import { getMovies } from './getMovies';
+// css file import
+import './Movies.css'
+// import { getMovies } from './getMovies';
 import axios from 'axios';
 export default class Movies extends Component {
     constructor() {
@@ -100,22 +102,22 @@ export default class Movies extends Component {
         this.state = {
             movies: [],
             currSearchText: '',
-            currPageNumber:1,
-            limit:4,
-            genres:[{_id:"123",name:"All Genres"}],
-            currGenre:"All Genres"
+            currPageNumber: 1,
+            limit: 4,
+            genres: [{ _id: "123", name: "All Genres" }],
+            currGenre: "All Genres"
         }
     }
 
-    async componentDidMount(){
+    async componentDidMount() {
         console.log("component did mount");
-        let getMovies=await axios.get("https://backend-react-movie.herokuapp.com/movies");
+        let getMovies = await axios.get("https://backend-react-movie.herokuapp.com/movies");
         // console.log(getMovies);
-        let allGenres=await axios.get("https://backend-react-movie.herokuapp.com/genres");
+        let allGenres = await axios.get("https://backend-react-movie.herokuapp.com/genres");
         // console.log(allGenres);
         this.setState({
-            movies:[...getMovies.data.movies],
-            genres:[...this.state.genres,...allGenres.data.genres]
+            movies: [...getMovies.data.movies],
+            genres: [...this.state.genres, ...allGenres.data.genres]
         })
     }
     handleChange = (e) => {
@@ -177,34 +179,35 @@ export default class Movies extends Component {
 
     }
 
-    pageChangeHandle=(pageNum)=>{
+    pageChangeHandle = (pageNum) => {
         // console.log("inpageHandler",pageNum);
-        this.setState({currPageNumber:pageNum});
+        this.setState({ currPageNumber: pageNum });
     }
 
-    handleLimit=(e)=>{
-        let className=e.target.className;
-        let nlimit=0;
-        let currLimit=this.state.limit;
-        if(className=='up btn btn-success'){
-            nlimit=currLimit+1;
+    handleLimit = (e) => {
+        let className = e.target.className;
+        let nlimit = 0;
+        let currLimit = this.state.limit;
+        if (className == 'up btn btn-success') {
+            nlimit = currLimit + 1;
         }
-        else if(className=='down btn btn-danger'){
-            nlimit=currLimit-1;
+        else if (className == 'down btn btn-danger') {
+            nlimit = currLimit - 1;
         }
 
-        this.setState({limit:nlimit});
+        this.setState({ limit: nlimit });
     }
 
-    handleGenreChange=(genre)=>{
-        this.setState({currGenre:genre});
+    handleGenreChange = (genre) => {
+        this.setState({ currGenre: genre });
     }
 
 
 
     render() {
         console.log('render');
-        let { movies, currSearchText, currPageNumber, limit, genres,currGenre} = this.state; //ES6 destructuring
+        let { movies, currSearchText, currPageNumber, limit, genres, currGenre } = this.state; //ES6 destructuring
+
         let filteredArr = [];
         if (currSearchText == '') {
             filteredArr = movies;
@@ -216,114 +219,125 @@ export default class Movies extends Component {
                 return title.includes(currSearchText.toLowerCase());
             })
         }
-        if(currGenre!="All Genres"){
-            filteredArr= filteredArr.filter((movieObj)=>{
-                return movieObj.genre.name==currGenre;
+        if (currGenre != "All Genres") {
+            filteredArr = filteredArr.filter((movieObj) => {
+                return movieObj.genre.name == currGenre;
             })
         }
         // no. of movies per page
         // let limit=4; 
         // console.log(currPageNumber);
-        let si=(currPageNumber-1)*limit;
-        let ei=si+limit;
-        let numberOfPages=Math.ceil(filteredArr.length/limit);
-        let numberOfPagesArr=[];
-        for(let i=1;i<=numberOfPages;i++){
+        let si = (currPageNumber - 1) * limit;
+        let ei = si + limit;
+        let numberOfPages = Math.ceil(filteredArr.length / limit);
+        let numberOfPagesArr = [];
+        for (let i = 1; i <= numberOfPages; i++) {
             numberOfPagesArr.push(i);
         }
-        let moviesPerPage=filteredArr.slice(si,ei);
+        let moviesPerPage = filteredArr.slice(si, ei);
         // console.log(numberOfPagesArr);
         // console.log(this.state.genres)
 
         return (
             //JSX
-            <div className='container'>
-                <div className='row'>
-                    <div className='col-3'>
-                        <ul className="list-group">
-                            {
-                                this.state.genres.map((genreObj)=>{
-                                    let className=this.state.currGenre==genreObj.name?"list-group-item active":"list-group-item";
-                                    return(
-                                        <li key={genreObj._id} className={className} onClick={()=>{this.handleGenreChange(genreObj.name)}} >{genreObj.name}</li>
-                                    )
-                                })
-                            }
-                        </ul>
-                    </div>
-                    <div className='col-9'>
-                        
-                        <input className="form-control me-2" type='search' value={this.state.currSearchText} onChange={this.handleChange} ></input>
-                        <div>
-                            <button className="up btn btn-success" type="button" onClick={this.handleLimit}>+</button>
-                            <span>{this.state.limit}</span>
-                            <button className="down btn btn-danger" onClick={this.handleLimit}>-</button>
-                        </div>
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Title</th>
-                                    <th scope="col">Genre</th>
-                                    <th scope="col">
-                                        <i className="fa fa-sort-asc" onClick={this.sortByStock} aria-hidden="true"></i>
-                                        Stock
-                                        <i className="fa fa-sort-desc" onClick={this.sortByStock} aria-hidden="true"></i>
-                                    </th>
-                                    <th scope="col">
-                                        <i className="fa fa-sort-asc" onClick={this.sortByRate} aria-hidden="true"></i>
-                                        Rate
-                                        <i className="fa fa-sort-desc" onClick={this.sortByRate} aria-hidden="true"></i>
-                                    </th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    moviesPerPage.map((movieObj) => {
-                                        return (
-                                            <tr key={movieObj._id} >
-                                                <td></td>
-                                                <td>{movieObj.title}</td>
-                                                <td>{movieObj.genre.name}</td>
-                                                <td>{movieObj.numberInStock}</td>
-                                                <td>{movieObj.dailyRentalRate}</td>
-                                                <td><button onClick={() => {
-                                                    this.onDelete(movieObj._id)
-                                                }} type="button" className="btn btn-danger">Delete</button></td>
+            <>
+                {
+                this.state.movies.length === 0 ?
+                 
+                        <div id="loader" className="nfLoader"></div>
+              
+                      :
+                        <div className='container'>
+
+
+                            <div className='row'>
+                                <div className='col-3'>
+                                    <ul className="list-group">
+                                        {
+                                            this.state.genres.map((genreObj) => {
+                                                let className = this.state.currGenre == genreObj.name ? "list-group-item active" : "list-group-item";
+                                                return (
+                                                    <li key={genreObj._id} className={className} onClick={() => { this.handleGenreChange(genreObj.name) }} >{genreObj.name}</li>
+                                                )
+                                            })
+                                        }
+                                    </ul>
+                                </div>
+                                <div className='col-9'>
+
+                                    <input className="form-control me-2" type='search' value={this.state.currSearchText} onChange={this.handleChange} ></input>
+                                    <div>
+                                        <button className="up btn btn-success" type="button" onClick={this.handleLimit}>+</button>
+                                        <span>{this.state.limit}</span>
+                                        <button className="down btn btn-danger" onClick={this.handleLimit}>-</button>
+                                    </div>
+                                    <table className="table">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">#</th>
+                                                <th scope="col">Title</th>
+                                                <th scope="col">Genre</th>
+                                                <th scope="col">
+                                                    <i className="fa fa-sort-asc" onClick={this.sortByStock} aria-hidden="true"></i>
+                                                    Stock
+                                                    <i className="fa fa-sort-desc" onClick={this.sortByStock} aria-hidden="true"></i>
+                                                </th>
+                                                <th scope="col">
+                                                    <i className="fa fa-sort-asc" onClick={this.sortByRate} aria-hidden="true"></i>
+                                                    Rate
+                                                    <i className="fa fa-sort-desc" onClick={this.sortByRate} aria-hidden="true"></i>
+                                                </th>
+                                                <th></th>
                                             </tr>
-                                        )
-                                    })
-                                }
-                            </tbody>
-                        </table>
+                                        </thead>
+                                        <tbody>
+                                            {
+                                                moviesPerPage.map((movieObj) => {
+                                                    return (
+                                                        <tr key={movieObj._id} >
+                                                            <td></td>
+                                                            <td>{movieObj.title}</td>
+                                                            <td>{movieObj.genre.name}</td>
+                                                            <td>{movieObj.numberInStock}</td>
+                                                            <td>{movieObj.dailyRentalRate}</td>
+                                                            <td><button onClick={() => {
+                                                                this.onDelete(movieObj._id)
+                                                            }} type="button" className="btn btn-danger">Delete</button></td>
+                                                        </tr>
+                                                    )
+                                                })
+                                            }
+                                        </tbody>
+                                    </table>
 
-                        {/* pagination */}
-                        <nav aria-label="...">
-                            <ul className="pagination">
-                                {
-                                numberOfPagesArr.map((pageNumber)=>{
-                                    
-                                    // console.log(pageNumber);
-                                    let className=pageNumber==currPageNumber?"page-item active":"page-item"
-                                    return(
-                                    <li key={pageNumber} className={className} onClick={()=>{this.pageChangeHandle(pageNumber)}}><span className="page-link">{pageNumber}</span></li>
-                                    )
+                                    {/* pagination */}
+                                    <nav aria-label="...">
+                                        <ul className="pagination">
+                                            {
+                                                numberOfPagesArr.map((pageNumber) => {
 
-                                })
-                                }
-                                {/* <li className="page-item"><span className="page-link">1</span></li>
+                                                    // console.log(pageNumber);
+                                                    let className = pageNumber == currPageNumber ? "page-item active" : "page-item"
+                                                    return (
+                                                        <li key={pageNumber} className={className} onClick={() => { this.pageChangeHandle(pageNumber) }}><span className="page-link">{pageNumber}</span></li>
+                                                    )
+
+                                                })
+                                            }
+                                            {/* <li className="page-item"><span className="page-link">1</span></li>
                                 <li className="page-item active" >
                                     <span className="page-link" aria-current="page">2</span>
                                 </li>
                                 <li className="page-item"><span className="page-link">3</span></li> */}
-                                
-                            </ul>
-                        </nav>
 
-                    </div>
-                </div>
-            </div>
+                                        </ul>
+                                    </nav>
+
+                                </div>
+                            </div>
+                        </div>
+                }
+            </>
         )
     }
 }
